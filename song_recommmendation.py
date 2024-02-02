9367dd2984a1cf7cc625919bd0e25830588e4cc8
현서 조 <whgustj0222@gmail.com>
2024-02-02 16:15:17 +0900

    # song_recommendation.py changed.

--- song_recommmendation.py
+++ song_recommmendation.py
@@ -8,6 +8,9 @@ import spotipy
 import time
 import sys
 import pprint
+import time
+import numpy as np
+import pandas as pd
 
 client_id="4063cdbe68ea499a92861b915ff0e537"
 client_secret="4d5c47c885634e32a745ca8e4e87912b"
@@ -68,4 +71,82 @@ for i in album_uris: #each album
     print("Album " + str(album_names[album_count]) + " songs has been added to spotify_albums dictionary")
     album_count+=1 #Updates album count once all tracks have been added
 
-pprint.pprint(spotify_albums, album_count)
+pprint.pprint(spotify_albums)
+
+
+
+def audio_features(album):
+    #Add new key-values to store audio features
+    spotify_albums[album]['acousticness'] = []
+    spotify_albums[album]['danceability'] = []
+    spotify_albums[album]['energy'] = []
+    spotify_albums[album]['instrumentalness'] = []
+    spotify_albums[album]['liveness'] = []
+    spotify_albums[album]['loudness'] = []
+    spotify_albums[album]['speechiness'] = []
+    spotify_albums[album]['tempo'] = []
+    spotify_albums[album]['valence'] = []
+    spotify_albums[album]['popularity'] = []
+    #create a track counter
+    track_count = 0
+    for track in spotify_albums[album]['uri']:
+        #pull audio features per track
+        features = sp.audio_features(track)
+        
+        #Append to relevant key-value
+        spotify_albums[album]['acousticness'].append(features[0]['acousticness'])
+        spotify_albums[album]['danceability'].append(features[0]['danceability'])
+        spotify_albums[album]['energy'].append(features[0]['energy'])
+        spotify_albums[album]['instrumentalness'].append(features[0]['instrumentalness'])
+        spotify_albums[album]['liveness'].append(features[0]['liveness'])
+        spotify_albums[album]['loudness'].append(features[0]['loudness'])
+        spotify_albums[album]['speechiness'].append(features[0]['speechiness'])
+        spotify_albums[album]['tempo'].append(features[0]['tempo'])
+        spotify_albums[album]['valence'].append(features[0]['valence'])
+        #popularity is stored elsewhere
+        pop = sp.track(track)
+        spotify_albums[album]['popularity'].append(pop['popularity'])
+        track_count+=1
+        
+
+sleep_min = 2
+sleep_max = 5
+start_time = time.time()
+request_count = 0
+for i in spotify_albums:
+    audio_features(i)
+    request_count+=1
+    if request_count % 5 == 0:
+        print(str(request_count) + " playlists completed")
+        time.sleep(np.random.uniform(sleep_min, sleep_max))
+        print('Loop #: {}'.format(request_count))
+        print('Elapsed Time: {} seconds'.format(time.time() - start_time))
+        
+pprint.pprint(spotify_albums)
+
+
+dic_df = {}
+dic_df['album'] = []
+dic_df['track_number'] = []
+dic_df['id'] = []
+dic_df['name'] = []
+dic_df['uri'] = []
+dic_df['acousticness'] = []
+dic_df['danceability'] = []
+dic_df['energy'] = []
+dic_df['instrumentalness'] = []
+dic_df['liveness'] = []
+dic_df['loudness'] = []
+dic_df['speechiness'] = []
+dic_df['tempo'] = []
+dic_df['valence'] = []
+dic_df['popularity'] = []
+for album in spotify_albums: 
+    for feature in spotify_albums[album]:
+        dic_df[feature].extend(spotify_albums[album][feature])
+        
+len(dic_df['album'])
+
+df = pd.DataFrame.from_dict(dic_df)
+df['artist'] = name
+df
