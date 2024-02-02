7cb0351cf9130fcb1701a229366a73949214ab88
johyeonseo222222 <99173997+johyeonseo222222@users.noreply.github.com>
2024-02-02 15:01:16 +0900

    Add files via upload

---
+++ abstract_user_playlists.py
@@ -0,0 +1,20 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from flask import Flask, redirect, request, session
+import requests
+import pprint
+
+
+client_id="4063cdbe68ea499a92861b915ff0e537"
+client_secret="4d5c47c885634e32a745ca8e4e87912b"
+redirect_uri = "http://127.0.0.1:5000/redirect"
+scope = 'playlist-read-private'
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri,scope=scope))
+
+
+# shows ueer playlists
+results = sp.current_user_playlists(limit=50)
+for i, item in enumerate(results['items']):
+    print("%d %s" % (i, item['name']))
+
+
